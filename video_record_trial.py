88208b9d705d98095d40d4f9ae18682321ee1de7
amunson27 <56043524+amunson27@users.noreply.github.com>
2020-08-06 15:16:06 -0400

    Add files via upload

---
+++ video_record_trial.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import picamera
+
+camera = picamera.PiCamera()
+camera.resolution = (640, 480)
+camera.start_recording('my_video.h264')
+camera.wait_recording(60)
+camera.stop_recording()
